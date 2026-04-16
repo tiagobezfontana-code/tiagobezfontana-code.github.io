58f9ccc38bd976893a68f92d896d2076f346f54a
tiagobezfontana-code <tiagobezfontana@gmail.com>
2026-04-16 16:22:05 -0400

    Update script.js

--- lab/test2/script.js
+++ lab/test2/script.js
@@ -1,1 +1,17 @@
 
+const lot = document.getElementById("lot")
+
+for(let i=1;i<=20;i++){
+
+const space=document.createElement("div")
+space.className="space"
+space.innerText="P"+i
+
+space.onclick=()=>{
+space.classList.toggle("reserved")
+}
+
+lot.appendChild(space)
+
+}
+
